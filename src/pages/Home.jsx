9918a2572921e3cc67eb58b17aea3df86cdf44be
niraihan <raihan14un@gmail.com>
2025-06-15 00:54:34 +0600

    Home Page Designe added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,21 @@ import UpcomingMarathonsSection from './UpcomingMarathonsSection';
 import WhyJoinSection from './WhyJoinSection';
 import HowToPrepareSection from './HowToPrepareSection';
 import useTitle from '../hooks/useTitle';
-
+import { motion } from "framer-motion";
 const Home = () => {
     useTitle("MarathonPro | Home");
     return (
         <div className="text-center mt-10">
-            <h1 className="text-3xl font-bold">Welcome to Marathon Management System</h1>
+            <motion.h1
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, type: "spring" }}
+      className="text-4xl md:text-5xl font-extrabold text-center my-8"
+    >
+      <span className="bg-gradient-to-r from-rose-500 via-amber-500 to-emerald-500 bg-clip-text text-transparent drop-shadow-lg animate-pulse">
+        Welcome to Marathon Management System
+      </span>
+    </motion.h1>
             <Banner></Banner>
             <div className='mt-10'>
                 <Marathons></Marathons>
@@ -21,7 +30,7 @@ const Home = () => {
             <div className='mt-10'>
                 <WhyJoinSection></WhyJoinSection>
             </div>
-             <div className='mt-10'>
+            <div className='mt-10'>
                 <HowToPrepareSection></HowToPrepareSection>
             </div>
         </div>
